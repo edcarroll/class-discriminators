9cee73f9f364b4ed3d909d0f094a9fd8c9dc30a3
Ed Carroll <ed@edcarroll.co.uk>
2017-07-24 23:36:30 +0100

    refactor: Moved configureGuardFactory return type into interface for better intellisence

--- src/configure-factory.ts
+++ src/configure-factory.ts
@@ -10,6 +10,51 @@ declare global {
 
 export type Guard<T, U extends T> = (guardee:T) => guardee is U;
 
+// tslint:disable-next-line:ext-variable-name
+export interface ConfiguredGuardFactory<T> {
+    <U extends T>(
+        t1:Class<U>)
+            :Guard<T, U>;
+
+    <U1 extends T, U2 extends T>(
+        t1:Class<U1>, t2:Class<U2>)
+            :Guard<T, U1 | U2>;
+
+    <U1 extends T, U2 extends T, U3 extends T>(
+        t1:Class<U1>, t2:Class<U2>, t3:Class<U3>)
+            :Guard<T, U1 | U2 | U3>;
+
+    <U1 extends T, U2 extends T, U3 extends T, U4 extends T>(
+        t1:Class<U1>, t2:Class<U2>, t3:Class<U3>, t4:Class<U4>)
+            :Guard<T, U1 | U2 | U3 | U4>;
+
+    <U1 extends T, U2 extends T, U3 extends T, U4 extends T, U5 extends T>(
+        t1:Class<U1>, t2:Class<U2>, t3:Class<U3>, t4:Class<U4>, t5:Class<U5>)
+            :Guard<T, U1 | U2 | U3 | U4 | U5>;
+
+    <U1 extends T, U2 extends T, U3 extends T, U4 extends T, U5 extends T, U6 extends T>(
+        t1:Class<U1>, t2:Class<U2>, t3:Class<U3>, t4:Class<U4>, t5:Class<U5>, t6:Class<U6>)
+            :Guard<T, U1 | U2 | U3 | U4 | U5 | U6>;
+
+    <U1 extends T, U2 extends T, U3 extends T, U4 extends T, U5 extends T, U6 extends T, U7 extends T>(
+        t1:Class<U1>, t2:Class<U2>, t3:Class<U3>, t4:Class<U4>, t5:Class<U5>, t6:Class<U6>, t7:Class<U7>)
+            :Guard<T, U1 | U2 | U3 | U4 | U5 | U6 | U7>;
+
+    <U1 extends T, U2 extends T, U3 extends T, U4 extends T, U5 extends T, U6 extends T, U7 extends T, U8 extends T>(
+        t1:Class<U1>, t2:Class<U2>, t3:Class<U3>, t4:Class<U4>, t5:Class<U5>, t6:Class<U6>, t7:Class<U7>, t8:Class<U8>)
+            :Guard<T, U1 | U2 | U3 | U4 | U5 | U6 | U7 | U8>;
+
+    <U1 extends T, U2 extends T, U3 extends T, U4 extends T, U5 extends T, U6 extends T, U7 extends T, U8 extends T, U9 extends T>(
+        t1:Class<U1>, t2:Class<U2>, t3:Class<U3>, t4:Class<U4>, t5:Class<U5>, t6:Class<U6>, t7:Class<U7>, t8:Class<U8>, t9:Class<U9>)
+            :Guard<T, U1 | U2 | U3 | U4 | U5 | U6 | U7 | U8 | U9>;
+
+    <U1 extends T, U2 extends T, U3 extends T, U4 extends T, U5 extends T, U6 extends T, U7 extends T, U8 extends T, U9 extends T,
+     U10 extends T>(
+        t1:Class<U1>, t2:Class<U2>, t3:Class<U3>, t4:Class<U4>, t5:Class<U5>, t6:Class<U6>, t7:Class<U7>, t8:Class<U8>, t9:Class<U9>,
+        t10:Class<U10>)
+            :Guard<T, U1 | U2 | U3 | U4 | U5 | U6 | U7 | U8 | U9 | U10>;
+}
+
 export function extractDiscriminators<T>(types:Class<T>[]):string[] {
     return types.map((t:DiscriminatedClass<T>) => {
         if (t.discriminatorValue) {
@@ -19,50 +64,8 @@ export function extractDiscriminators<T>(types:Class<T>[]):string[] {
     });
 }
 
-// tslint:disable-next-line:typedef
-export function configureGuardFactory<T>(discriminatorProperty:keyof T) {
-    function configuredGuardFactory<U extends T>(
-    t1:Class<U>):Guard<T, U>;
-
-    function configuredGuardFactory<U1 extends T, U2 extends T>(
-    t1:Class<U1>, t2:Class<U2>):Guard<T, U1 | U2>;
-
-    function configuredGuardFactory<U1 extends T, U2 extends T, U3 extends T>(
-    t1:Class<U1>, t2:Class<U2>, t3:Class<U3>):Guard<T, U1 | U2 | U3>;
-
-    function configuredGuardFactory<U1 extends T, U2 extends T, U3 extends T, U4 extends T>(
-    t1:Class<U1>, t2:Class<U2>, t3:Class<U3>, t4:Class<U4>):Guard<T, U1 | U2 | U3 | U4>;
-
-    function configuredGuardFactory<U1 extends T, U2 extends T, U3 extends T, U4 extends T, U5 extends T>(
-    t1:Class<U1>, t2:Class<U2>, t3:Class<U3>, t4:Class<U4>, t5:Class<U5>):Guard<T, U1 | U2 | U3 | U4 | U5>;
-
-    function configuredGuardFactory<U1 extends T, U2 extends T, U3 extends T, U4 extends T, U5 extends T, U6 extends T>(
-    t1:Class<U1>, t2:Class<U2>, t3:Class<U3>, t4:Class<U4>, t5:Class<U5>, t6:Class<U6>):Guard<T, U1 | U2 | U3 | U4 | U5 | U6>;
-
-    function configuredGuardFactory<U1 extends T, U2 extends T, U3 extends T, U4 extends T, U5 extends T, U6 extends T,
-                                U7 extends T>(
-    t1:Class<U1>, t2:Class<U2>, t3:Class<U3>, t4:Class<U4>, t5:Class<U5>, t6:Class<U6>,
-    t7:Class<U7>):Guard<T, U1 | U2 | U3 | U4 | U5 | U6 | U7>;
-
-    function configuredGuardFactory<U1 extends T, U2 extends T, U3 extends T, U4 extends T, U5 extends T, U6 extends T,
-                                U7 extends T, U8 extends T>(
-    t1:Class<U1>, t2:Class<U2>, t3:Class<U3>, t4:Class<U4>, t5:Class<U5>, t6:Class<U6>,
-    t7:Class<U7>, t8:Class<U8>):Guard<T, U1 | U2 | U3 | U4 | U5 | U6 | U7 | U8>;
-
-    function configuredGuardFactory<U1 extends T, U2 extends T, U3 extends T, U4 extends T, U5 extends T, U6 extends T,
-                                U7 extends T, U8 extends T, U9 extends T>(
-    t1:Class<U1>, t2:Class<U2>, t3:Class<U3>, t4:Class<U4>, t5:Class<U5>, t6:Class<U6>,
-    t7:Class<U7>, t8:Class<U8>, t9:Class<U9>):Guard<T, U1 | U2 | U3 | U4 | U5 | U6 | U7 | U8 | U9>;
-
-    function configuredGuardFactory<U1 extends T, U2 extends T, U3 extends T, U4 extends T, U5 extends T, U6 extends T,
-                                U7 extends T, U8 extends T, U9 extends T, U10 extends T>(
-    t1:Class<U1>, t2:Class<U2>, t3:Class<U3>, t4:Class<U4>, t5:Class<U5>, t6:Class<U6>,
-    t7:Class<U7>, t8:Class<U8>, t9:Class<U9>, t10:Class<U10>):Guard<T, U1 | U2 | U3 | U4 | U5 | U6 | U7 | U8 | U9 | U10>;
-
-    function configuredGuardFactory<U extends T>(...types:Class<U>[]):Guard<T, U> {
-        return (guardee:T):guardee is U =>
+export function configureGuardFactory<T>(discriminatorProperty:keyof T):ConfiguredGuardFactory<T> {
+    return <U extends T>(...types:Class<U>[]) =>
+        (guardee:T):guardee is U =>
             extractDiscriminators(types).indexOf(guardee[discriminatorProperty].toString()) !== -1;
-    }
-
-    return configuredGuardFactory;
 }
